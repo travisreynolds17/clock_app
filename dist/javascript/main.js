00e0be4524343621467c4ad61492b1a7ac546c54
Travis Reynolds <travisreynolds17@gmail.com>
2018-09-24 21:52:44 -0400

    implement background changes/ clock updates

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -2,7 +2,15 @@
 // Optionally, we want different pictures to appear in the back of the clockface depending on time.
 // Because of GH Pages limitations, we must do this without form submissions
 
-var heading = document.querySelector(".heading");
+// DOM selection
+const heading = document.querySelector(".heading");
+const clockFace = document.querySelector(".clock-face");
+
+// array of class names to be randomized for clockface picture
+const picsClass = ["pic1", "pic2", "pic3", "pic4", "pic5", "pic6"];
+// time in seconds between clockface background changes
+let picsTime = 10;
+let picsInit = 0;
 
 // We will write a function that places an HTML element template at a specified position (node) in the HTML markup
 function render(template, node) {
@@ -15,10 +23,44 @@ function render(template, node) {
   }
 }
 
-// Write a function that updates current date object every second
+//create a function that ensures each hour, minute, and second variable is two digits even if less than 10, then convert to string
+function doubleDigits(increment) {
+  if (increment < 10) {
+    increment = "0" + increment.toString();
+    return increment;
+  } else {
+    increment.toString();
+    return increment;
+  }
+}
+
+// write a function that adds new picture to clock face
+function clockPic() {
+  var selector = 100;
+  while (selector > picsClass.length - 1) {
+    selector = Math.floor(Math.random() * 10);
+  }
+  for (var i = 0; i < picsClass.length; i++) {
+    clockFace.classList.remove(picsClass[i]);
+  }
+  clockFace.classList.add(picsClass[selector]);
+  // reset timer between changes
+  picsInit = 0;
+}
+
+// Write a function that updates current date object every second and calls picture changing function
 function tick() {
   let currentTime = new Date();
-  render("It is now " + currentTime.toString(), heading);
+  let setHours = doubleDigits(currentTime.getHours());
+  let setMinutes = doubleDigits(currentTime.getMinutes());
+  let setSeconds = doubleDigits(currentTime.getSeconds());
+
+  let time = setHours + ":" + setMinutes + ":" + setSeconds;
+  render("It is now " + time.toString(), clockFace);
+  picsInit++;
+  if (picsInit == picsTime) {
+    clockPic();
+  }
 }
 
 window.setInterval(tick, 1000);
