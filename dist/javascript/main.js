b9cb358df47bba2471abfb260a9f8312b61c0c7c
Travis Reynolds <travisreynolds17@gmail.com>
2018-09-25 23:43:58 -0400

    timer functions

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -8,6 +8,9 @@ const clockFace = document.querySelector(".clock-face");
 const clockRadio = document.querySelector(".clock-settings");
 const radioButtons = document.querySelectorAll(".clock-settings input");
 const reset = document.querySelector(".reset");
+const setTimerBtn = document.querySelector(".timer");
+const setStopwatchBtn = document.querySelector(".stopwatch");
+const setClockBtn = document.querySelector(".clock");
 // selectors for dropdown menus for timer and stopwatch
 const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
@@ -19,6 +22,9 @@ const picsClass = ["pic1", "pic2", "pic3", "pic4", "pic5", "pic6"];
 let picsTime = 10;
 let picsInit = 0;
 let ticking = true;
+let downTicking = false;
+let timerSet = false;
+let stopwatchSet = false;
 
 let radioDefault = true;
 
@@ -28,6 +34,9 @@ let defaultDisplay = "00:00:00";
 
 function init() {
   reset.addEventListener("click", resetClock);
+  setTimerBtn.addEventListener("click", goTimer);
+  setStopwatchBtn.addEventListener("click", setStopwatch);
+  setClockBtn.addEventListener("click", setClock);
   window.setInterval(tick, 1000);
   populateSelects();
 }
@@ -87,6 +96,11 @@ function tick() {
 
 function resetClock() {
   ticking = false;
+  stopwatchSet = false;
+  downTicking = false;
+  timerSet = false;
+  timerStarted = false;
+
   clockFace.innerHTML = defaultDisplay;
   radioButtons.forEach(item => {
     item.checked = false;
@@ -94,26 +108,95 @@ function resetClock() {
 }
 
 function populateSelects() {
-  let i = 0;
+  let i = 23;
   function getShell() {
     let shell = "<option value = '" + i + "'>" + i + "</option>";
     shell.toString();
-    console.log(shell);
     return shell;
   }
-  while (i < 24) {
+  while (i >= 0) {
     let shell = getShell();
     hours.insertAdjacentHTML("afterBegin", shell);
-    i++;
+    i--;
   }
-  while (i < 59) {
+
+  i = 59;
+
+  while (i > 0) {
     let shell = getShell();
-    i = 0;
     minutes.insertAdjacentHTML("afterBegin", shell);
-    i = 0;
     seconds.insertAdjacentHTML("afterBegin", shell);
-    i++;
+    i--;
   }
 }
+
+function goTimer() {
+  if (!timerSet) {
+    setTimer();
+  } else if (!timerStarted) {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+}
+function setTimer() {
+  resetClock();
+  let selectHours = doubleDigits(hours.value);
+  let selectMinutes = doubleDigits(minutes.value);
+  let selectSeconds = doubleDigits(seconds.value);
+
+  let time =
+    selectHours.toString() +
+    ":" +
+    selectMinutes.toString() +
+    ":" +
+    selectSeconds.toString();
+
+  render(time, clockFace);
+
+  timerSet = true;
+  setTimerBtn.innerHTML = "Start Timer";
+}
+
+function startTimer() {
+  window.setInterval(downTick, 1000);
+  downTicking = true;
+  timerStarted = true;
+  setTimerBtn.innerHTML = "Stop Timer";
+}
+
+function stopTimer() {
+  downTicking = false;
+  timerSet = false;
+  timerStarted = false;
+}
+function downTick(selectSeconds, selectMinutes, selectHours) {
+  if (downTicking) {
+    selectSeconds--;
+    if (selectSeconds < 0) {
+      selectMinutes--;
+      if (selectMinutes < 0) {
+        selectHours--;
+      }
+    }
+    let time =
+      doubleDigits(selectHours).toString() +
+      ":" +
+      doubleDigits(selectMinutes).toString() +
+      ":" +
+      doubleDigits(selectSeconds).toString();
+
+    render(time, clockFace);
+
+    if (time === "00:00:00") {
+      render("TIME", clockFace);
+      downTicking = false;
+    }
+  }
+}
+
+function setStopwatch() {}
+
+function setClock() {}
 
 init();
